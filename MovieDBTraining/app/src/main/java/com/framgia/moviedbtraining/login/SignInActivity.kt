b82a01c11198c1a518feaf6b4aa0e28a613d8539
nambingo <phamducnamtb@gmail.com>
2017-07-06 10:00:40 +0700

    create sub movie, login

---
+++ MovieDBTraining/app/src/main/java/com/framgia/moviedbtraining/login/SignInActivity.kt
@@ -0,0 +1,16 @@
+package com.framgia.moviedbtraining.login
+
+import android.app.Activity
+import android.os.Bundle
+import com.framgia.moviedbtraining.R
+
+/**
+ * Created by FRAMGIA\pham.duc.nam on 06/07/2017.
+ */
+
+class SignInActivity : Activity(){
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_sign_in)
+  }
+}
